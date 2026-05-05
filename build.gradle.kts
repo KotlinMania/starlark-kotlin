abad40fa6c2fc3d9d553d6326e9f1d9b5c8c27a5
Sydney Renee <sydney@solace.ofharmony.ai>
2026-05-05 13:25:17 -0700

    Remove deprecated Apple x64 targets

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,21 +45,9 @@ kotlin {
             xcf.add(this)
         }
     }
-    macosX64 {
-        binaries.framework {
-            baseName = "Starlark"
-            xcf.add(this)
-        }
-    }
     linuxX64()
     mingwX64()
     iosArm64 {
-        binaries.framework {
-            baseName = "Starlark"
-            xcf.add(this)
-        }
-    }
-    iosX64 {
         binaries.framework {
             baseName = "Starlark"
             xcf.add(this)
@@ -130,7 +118,6 @@ rootProject.extensions.configure<YarnRootExtension>("kotlinYarn") {
     resolution("qs", "6.15.1")
     resolution("socket.io-parser", "4.2.6")
 }
-
 
 mavenPublishing {
     publishToMavenCentral()
