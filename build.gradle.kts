7d8cf85ce9ed183bf2e85573904194a365f217f0
Sydney Renee <sydney@solace.ofharmony.ai>
2026-05-06 09:53:26 -0700

    Revert: keep Sydney's working yarn-via-corepack CI pattern

--- build.gradle.kts
+++ build.gradle.kts
@@ -111,11 +111,11 @@ kotlin {
 }
 
 rootProject.extensions.configure<NodeJsEnvSpec>("kotlinNodeJsSpec") {
-    version.set("24.13.0")
+    version.set("22.22.2")
 }
 
 rootProject.extensions.configure<WasmNodeJsEnvSpec>("kotlinWasmNodeJsSpec") {
-    version.set("24.13.0")
+    version.set("22.22.2")
 }
 
 rootProject.extensions.configure<YarnRootEnvSpec>("kotlinYarnSpec") {
